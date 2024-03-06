264e0b0e332b0ace9660d5457fa9e1e5775f9e97
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-06 12:33:15 +0100

    feat: home nav login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import FooterArea from './components/FooterArea.vue';
 import HomeArea from './components/HomeArea.vue';
+import LoginArea from './components/LoginArea.vue';
 </script>
 
 <template>
+  <LoginArea />
   <HomeArea />
   <FooterArea />
 </template>
---
+++ src/components/LoginArea.vue
@@ -0,0 +1,48 @@
+<!--JS-->
+
+<script>
+export default {}
+</script>
+
+<!--/JS-->
+
+<!--HTML-->
+
+<template>
+    <header class="d-flex align-items-center">
+        <div class="container d-flex justify-content-between">
+
+            <div class="d-flex">
+                <!--logo-->
+                <div class="logo pt-1">
+                    <img src="../../public/img/logo.jpeg" alt="Logo">
+                </div>
+                <!--/logo-->
+                <ul class="d-flex align-items-center ps-3">
+                    <li>
+                        Home
+                    </li>
+                </ul>
+            </div>
+
+
+            <!--login-register-->
+            <ul class="d-flex align-items-center">
+                <li class="pe-3">
+                    <a class="text-dark" href="">Login</a>
+                </li>
+                <li class="pe-2">
+                    <a class="text-dark" href="">Register</a>
+                </li>
+            </ul>
+            <!--/login-register-->
+        </div>
+    </header>
+</template>
+
+<!--/HTML-->
+
+<!--CSS-->
+
+<style scoped lang="scss"></style>
+<!--/CSS-->
