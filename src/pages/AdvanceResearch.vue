7e3cd1496733b9b3c53c02e496274e202e2a6531
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-03-08 01:20:48 +0100

    feat: style ristoranti e piatti

--- src/pages/AdvanceResearch.vue
+++ src/pages/AdvanceResearch.vue
@@ -31,11 +31,12 @@ export default {
 <!--HTML-->
 
 <template>
-    <section class="container">
+    <section class="container in-b">
+        <h2 class="mt-3 py-2 px-3 btn-org">Ristoranti</h2>
         <div class="d-flex flex-wrap">
             <!-- Itera su ciascun tipo nell'array types -->
-            <router-link class="type-card" role="button" @click="setRestaurant(item.id)" 
-                v-for="item in restaurants" :to="{ name: RestaurantArea, path: '/restaurants/'+item.id}" >
+            <router-link class="type-card rounded p-3 my-3" role="button" @click="setRestaurant(item.id)"
+                v-for="item in restaurants" :to="{ name: RestaurantArea, path: '/restaurants/' + item.id }">
                 <img :src="item.image" class="card-img-top" :alt="item.name">
                 <div class="card-body">
                     <div class="card-text text-center">{{ item.name }}</div>
