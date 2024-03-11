b556e7e5ef19876771269887f865d0e873407eb0
RiccardoImperato <riccardo.imperato@libero.it>
2024-03-11 16:24:06 +0100

    fix: pagina carrello

--- src/pages/RestaurantArea.vue
+++ src/pages/RestaurantArea.vue
@@ -15,22 +15,25 @@ export default {
     },
     methods: {
         addToCart(product) {
+            console.log(product);
             const existingProduct = this.cart.find(item => item.id === product.id);
 
             if (existingProduct) {
-                existingProduct.quantity += 1;
+                existingProduct.quantity += product.quantity;
             } else {
-                this.cart.push({ ...product, quantity: 1 });
+                this.cart.push({ ...product, quantity: product.quantity });
             }
             localStorage.setItem('cart', JSON.stringify(this.cart));
             console.log(this.cart)
         },
+
         getRestaurant() {
             axios.get('http://127.0.0.1:8000/api/restaurants/' + store.restaurant_id).then((response) => {
                 this.restaurant = response.data.results;
                 console.log(response);
             });
         },
+
     },
     created() {
         this.getRestaurant();
@@ -70,42 +73,13 @@ export default {
                 <div>
                     <span class="fs.secondary me-2 mt-3 mb-2"><strong>Prezzo:</strong></span>{{ item.price }}€
                 </div>
-                <!-- <div>
-                    <input type="number" v-model.number="dish.quantity" :min="1" placeholder="Quantità"
-                        @input="validateQuantity(dish)">
-                    <button class="btn btn-org mt-3" @click="addToCart(dish)">Aggiungi al carrello</button>
-                </div> -->
+                <input type="number" min="1" v-model="item.quantity" class="form-control mt-3 mb-3">
                 <div>
                     <button @click="addToCart(item)" class="btn btn-org">Aggiungi al carrello</button>
                 </div>
             </div>
         </div>
     </section>
-
-    <!-- <section class="container in-b">
-        <h2 class="mt-3 py-2 px-3 btn-org">Menù</h2>
-
-        <div class="d-flex flex-wrap">
-            <div class="restaurant-card d-flex flex-column gap-3 rounded p-3 my-3"
-                v-for="(dish, index) in restaurant.dishes" :key="index">
-                <div>
-                    <input type="checkbox" v-model="dish.selected" :id="'dish_' + index">
-                    <label class="card-body" :for="'dish_' + index"><img class="card-img-top" :src="dish.image"
-                            :alt="dish.name"> {{ dish.name }} - {{ dish.price }}€</label>
-                </div>
-                <div>
-                    <button @click="addToCart(item)" class="btn btn-org">Acquista</button>
-
-                    <div class="mt-auto">
-                        <input type="number" v-model.number="dish.quantity" :min="1" placeholder="Quantità"
-                            @input="validateQuantity(dish)">
-                        <button class="btn btn-org mt-3" @click="addToCart(dish)">Aggiungi al carrello</button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section> -->
 </template>
 
 <!--/HTML-->
