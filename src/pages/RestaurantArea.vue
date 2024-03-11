94e92ddbbfdec2eddf04bc4a7feb5b2636315c1c
cosimogiove <cosimo18497@gmail.com>
2024-03-11 17:14:33 +0100

    feat: torna indietro

--- src/pages/RestaurantArea.vue
+++ src/pages/RestaurantArea.vue
@@ -14,6 +14,10 @@ export default {
         }
     },
     methods: {
+        clearcart() {
+            localStorage.removeItem('cart');
+            this.cart = [];
+        },
         addToCart(product) {
             console.log(product);
             const existingProduct = this.cart.find(item => item.id === product.id);
@@ -28,7 +32,7 @@ export default {
         },
 
         getRestaurant() {
-            axios.get('http://127.0.0.1:8000/api/restaurants/' + store.restaurant_id).then((response) => {
+            axios.get('http://localhost:8000/api/restaurants/' + store.restaurant_id).then((response) => {
                 this.restaurant = response.data.results;
                 console.log(response);
             });
@@ -47,7 +51,31 @@ export default {
 
 <template>
     <section class="container in-b stylesection">
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  torna indietro
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">torna indietro</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        sei sicuro di voler tornare indietro e svuotare il carrello ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">chiudi</button>
+        <a @click="clearcart()" class="btn btn-secondary" href="/">svuota  e torna indietro</a>
+      </div>
+    </div>
+  </div>
+</div>
         <div class="d-flex justify-content-between">
+
             <div>
                 <h2 class="mt-3 py-2 px-3 btn-org">Menù</h2>
             </div>
@@ -88,6 +116,9 @@ export default {
 
 <style scoped lang="scss">
 .stylesection {
+    margin-top: 100px;
+}
+.modal-dialog{
     margin-top: 100px;
 }
 </style>
