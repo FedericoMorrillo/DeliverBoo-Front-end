737ea75db2b41d9f00806ca00f1dc8b744355831
RiccardoImperato <riccardo.imperato@libero.it>
2024-03-11 10:45:38 +0100

    feat: carrello e piatti

---
+++ src/components/Cart.vue
@@ -0,0 +1,225 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'Cart',
+    data() {
+        return {
+            showForm: false, // Inizializza la visibilità del modulo a falso
+            // dishes: [],
+            cart: [], // Inizializza il carrello come un array vuoto
+            selectedProductIndex: null, // Indice del prodotto selezionato dall'utente
+            quantity: 1, // Inizializza la quantità predefinita del prodotto
+            total: 0, // Inizializza il totale dei prodotti nel carrello
+            userData: {
+                name: '',
+                surname: '',
+                phone: '',
+                email: '',
+                address: ''
+            }
+        };
+    },
+    methods: {
+        getDishes() {
+            axios.get('http://localhost:8000/api/restaurants/1dishes').then((response) => {
+                this.dishes = response.data.results.dishes;
+            });
+        },
+        showUserDataForm() {
+            if (this.cart.length > 0) {
+                this.showForm = true; // Mostra il modulo dei dati dell'utente quando il pulsante "Checkout" viene premuto
+            }
+        },
+        addToCart(selectedDish) {
+            // Aggiunge il piatto selezionato al carrello solo se è selezionato e la quantità è maggiore di 0
+            if (selectedDish.selected && selectedDish.quantity > 0) {
+                this.cart.push({ item: selectedDish.name, quantity: selectedDish.quantity, price: selectedDish.price, image: selectedDish.image });
+                selectedDish.selected = false; // Resetta la selezione del piatto
+                selectedDish.quantity = 1; // Resetta la quantità del piatto
+                this.saveCart(); // Salva il carrello in localStorage
+            }
+        },
+        validateQuantity(dish) {
+            // Imposta la quantità minima a 1 se l'utente inserisce un valore minore
+            if (dish.quantity < 1) {
+                dish.quantity = 1;
+            }
+        },
+        saveCart() {
+            // Salva il carrello in localStorage
+            localStorage.setItem('cart', JSON.stringify(this.cart));
+            // Calcola il totale dei prodotti nel carrello
+            this.calculateTotal();
+        },
+        removeFromCart(index) {
+            // Rimuove un elemento dal carrello
+            this.cart.splice(index, 1);
+            this.saveCart(); // Salva il carrello in localStorage
+        },
+        updateQuantity() {
+            // Aggiorna la quantità di un elemento nel carrello
+            this.saveCart(); // Salva il carrello in localStorage
+        },
+        clearCart() {
+            // Svuota completamente il carrello
+            this.cart = [];
+            localStorage.removeItem('cart'); // Rimuovi il carrello da localStorage
+            this.calculateTotal(); // Aggiorna il totale
+        },
+        calculateTotal() {
+            // Calcola il totale dei prodotti nel carrello
+            this.total = this.cart.reduce((acc, item) => acc + (item.price * item.quantity || 0), 0);
+        },
+        validateUserData() {
+            return (
+                this.userData.name !== '' &&
+                this.userData.surname !== '' &&
+                this.userData.phone !== '' &&
+                this.userData.email !== '' &&
+                this.userData.address !== ''
+            );
+        },
+
+        clearUserData() {
+            this.userData = {
+                name: '',
+                surname: '',
+                phone: '',
+                email: '',
+                address: ''
+            };
+        },
+        confirmOrder() {
+            // Assicurati che il carrello non sia vuoto e che tutti i campi utente siano compilati
+            if (this.cart.length > 0 && this.validateUserData()) {
+                const orderData = {
+                    cart: this.cart,
+                    total: this.total,
+                    userData: this.userData
+                };
+                console.log(orderData);
+                this.clearCart();
+                this.clearUserData();
+                this.showForm = false;
+                // axios.post('http://localhost:8000/api/orders', orderData)
+                //     .then(response => {
+                //         // Il server ha confermato l'ordine e ha salvato i dati nel database
+                //         console.log('Ordine confermato:', response.data);
+                //         // Puoi resettare il carrello e i dati utente dopo che l'ordine è stato confermato
+                //         this.clearCart();
+                //         this.clearUserData();
+                //     })
+                //     .catch(error => {
+                //         console.error('Errore durante la conferma dell\'ordine:', error);
+                //     });
+            } else {
+                console.warn('Impossibile confermare un ordine vuoto o con dati utente incompleti.');
+            }
+        },
+    },
+    created() {
+        // this.getDishes();
+
+        // Carica il carrello dal localStorage all'avvio dell'applicazione
+        const savedCart = localStorage.getItem('cart');
+        if (savedCart) {
+            this.cart = JSON.parse(savedCart);
+            this.calculateTotal(); // Calcola il totale dei prodotti nel carrello
+        };
+    }
+}
+</script>
+
+<template>
+    <div class="cart">
+        <h2>Carrello</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Img</th>
+                    <th scope="col">Piatto</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Q.ta</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in cart" :key="index">
+                    <td><img :src="item.image" :alt="item.name"></td>
+                    <td>{{ item.item }}</td>
+                    <td>{{ item.price }} &euro;</td>
+                    <td><input type="number" v-model.number="item.quantity" min="1" @change="updateQuantity(index)">
+                    </td>
+                    <td><button class="btn btn-org" @click="removeFromCart(index)">Rimuovi</button></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Mostra il totale del carrello -->
+        <p>Totale: {{ total.toFixed(2) }} &euro;</p>
+
+        <!-- Pulsante per svuotare completamente il carrello -->
+        <button class="btn btn-org" @click="clearCart">Svuota Carrello</button>
+        <!-- Bottone Checkout -->
+        <button class="btn btn-org" @click="showUserDataForm">Checkout</button>
+
+        <!-- Dati utente -->
+        <form v-if="showForm" @submit.prevent="confirmOrder">
+            <h2>Dati dell'utente</h2>
+            <div class="user-data">
+                <label for="name">Nome:</label>
+                <input type="text" id="name" v-model="userData.name" required>
+            </div>
+            <div class="user-data">
+                <label for="surname">Cognome:</label>
+                <input type="text" id="surname" v-model="userData.surname" required>
+            </div>
+            <div class="user-data">
+                <label for="phone">Telefono:</label>
+                <input type="text" id="phone" v-model="userData.phone" required>
+            </div>
+            <div class="user-data">
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="userData.email" required>
+            </div>
+            <div class="user-data">
+                <label for="address">Indirizzo:</label>
+                <input type="text" id="address" v-model="userData.address" required>
+            </div>
+            <!-- Pulsante per confermare ordine -->
+            <button class="btn btn-org" type="submit">Conferma Ordine</button>
+        </form>
+
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+img {
+    height: 25px;
+    width: 25px;
+}
+
+form {
+    width: 250px;
+
+    .user-data {
+        margin: 10px 0;
+        display: flex;
+        justify-content: space-between;
+    }
+
+}
+
+
+.dishes {
+    width: 500px;
+    margin-top: 50px;
+
+    .dish {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+}
+</style>
--- src/pages/RestaurantArea.vue
+++ src/pages/RestaurantArea.vue
@@ -3,11 +3,16 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import Cart from '../components/Cart.vue';
+
 export default {
     data() {
         return {
             'restaurant': {}
         }
+    },
+    components: {
+        Cart
     },
     methods: {
         getRestaurant() {
@@ -30,21 +35,27 @@ export default {
 <template>
     <section class="container in-b">
         <h2 class="mt-3 py-2 px-3 btn-org">Menù</h2>
+        <Cart />
         <div class="d-flex flex-wrap">
-            <!-- Itera su ciascun tipo nell'array types -->
-            <div class="restaurant-card rounded p-3 my-3" v-for="item in restaurant.dishes">
-                <img :src="item.image" class="card-img-top" :alt="item.name">
-                <div class="card-body">
-                    <div class="card-text text-center"><strong>{{ item.name }}</strong></div>
-                </div>
+            <!-- Lista dei piatti come caselle di controllo -->
+            <div class="restaurant-card d-flex flex-column gap-3 rounded p-3 my-3"
+                v-for="(dish, index) in restaurant.dishes" :key="index">
                 <div>
-                    <span class="fs.secondary me-2 mt-3 mb-2"><strong>Prezzo:</strong></span>{{ item.price }}€
+                    <input type="checkbox" v-model="dish.selected" :id="'dish_' + index">
+                    <label class="card-body" :for="'dish_' + index"><img class="card-img-top" :src="dish.image"
+                            :alt="dish.name"> {{ dish.name }} - {{ dish.price }}€</label>
                 </div>
-                <div>
-                    <button class="btn btn-org">Acquista</button>
+
+                <div class="mt-auto">
+                    <!-- Input per selezionare la quantità dei piatti -->
+                    <input type="number" v-model.number="dish.quantity" :min="1" placeholder="Quantità"
+                        @input="validateQuantity(dish)">
+                    <button class="btn btn-org mt-3" @click="addToCart(dish)">Aggiungi al carrello</button>
                 </div>
+
             </div>
         </div>
+
     </section>
 </template>
 
