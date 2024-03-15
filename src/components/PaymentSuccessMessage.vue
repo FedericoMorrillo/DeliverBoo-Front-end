09f72b53bb704acf5d8c8265a214b795e91431f6
RiccardoImperato <riccardo.imperato@libero.it>
2024-03-15 09:57:35 +0100

    feat: conferma pagamento e toast per piatti

---
+++ src/components/PaymentSuccessMessage.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: {
+        message: String
+    },
+    methods: {
+        hideMessage() {
+            this.$emit('hide-message');
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="card mt-4 message">
+        <div class="card-body d-flex justify-content-center align-items-center gap-3">
+            <h3>{{ message }}</h3>
+            <a href="/" role="button" class="btn btn-org">Torna alla Home</a>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
