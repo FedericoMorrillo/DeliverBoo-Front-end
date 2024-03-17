c04bdceda26d650b8ed0022aa7e016eb2483f8d0
RiccardoImperato <riccardo.imperato@libero.it>
2024-03-17 16:07:29 +0100

    fix: messaggio pagamento

--- src/components/PaymentSuccessMessage.vue
+++ src/components/PaymentSuccessMessage.vue
@@ -12,11 +12,9 @@ export default {
 </script>
 
 <template>
-    <div class="card mt-4 mx-auto message">
-        <div class="card-body">
-            <h3 class="text-center">{{ message }}</h3>
-            <a href="/" role="button" class="btn custom_btn" @click="hideMessage">Torna alla Home</a>
-        </div>
+    <div class="my-5 mx-auto message text-center">
+        <h3 class=" alert alert-success" role="alert">{{ message }}</h3>
+        <a href="/" role="button" class="btn custom_btn" @click="hideMessage">Torna alla Home</a>
     </div>
 </template>
 
